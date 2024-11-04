f116123d807b63edeb33c2b2a821d4c347137430
Ellen McCullough <ellen.mccullough@kaplan.com>
2024-11-04 12:12:22 +0000

    added text to practise file explaining exercise

--- Git_practise.py
+++ Git_practise.py
@@ -5,3 +5,10 @@ Created on Mon Nov  4 11:38:47 2024
 @author: ellen
 """
 
+
+"""
+
+This is a test file where I will put some text, edit the text and practise using 
+git commands in gitbash to help me understand how it all works"""
+
+
